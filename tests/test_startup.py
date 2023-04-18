5177e254e0588ae8a33f1726fd4dc11b49d41ca0
ipeterov <ipeterov1@gmail.com>
2023-04-18 15:48:50 +0400

    Set chrome options in the new python test

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -3,11 +3,19 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-@pytest.mark.selenium
-def test_login_page_is_visible():
-    driver = webdriver.Chrome()
+@pytest.fixture
+def driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--nogpu")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(2)
+    return driver
 
+
+@pytest.mark.selenium
+def test_login_page_is_visible(driver):
     driver.get('http://localhost')
     email_field = driver.find_element(By.NAME, 'email')
     email_field.send_keys('test@example.com')
